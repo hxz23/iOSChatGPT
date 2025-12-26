aeefd0cc80d98639ba782fa79c577c178fad0344
haoxuezhi <haoxz_developer@163.com>
2025-12-26 10:16:37 +0800

    feat: init project

---
+++ iOSChatGPT/iOSChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChatGPTApp.swift
+//  iOSChatGPT
+//
+//  Created by 郝学智 on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
